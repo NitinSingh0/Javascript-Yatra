b6c15807e255800caf2a78ab2eb5441b4246eaad
NitinSingh0 <31nitinthakur@gmail.com>
2025-05-02 15:22:40 +0530

    Lecture-03

---
+++ Lecture-03/script.js
@@ -0,0 +1,15 @@
+// alert("Hello World!");
+// let course = "Javascript Journey";
+// if (course === "Javascript Journey") alert("JS is Fun");
+
+// console.log(40 + 20 + 11 - 3 * 2);
+
+// console.log("nitin");
+
+let firstName = "Nitin";
+let a = "Nitin";
+const PI = 3.1415;
+let lastName = "nitin";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
