b3efc542301b1b7f5e292af8154d29b6eb23a054
NitinSingh0 <31nitinthakur@gmail.com>
2025-05-05 20:01:24 +0530

    Lecture-04 and Lecture-05

---
+++ Lecture-04/script.js
@@ -0,0 +1,31 @@
+//javascript code
+let firstName = "Nitin";
+console.log(firstName);
+console.log(typeof firstName);
+ 
+let marks = true;
+console.log(typeof marks);
+
+let hasPan = null;
+console.log(hasPan);
+console.log(typeof hasPan);
+
+//object
+let person = {
+    "firstName": "Nitin",
+    "age":20,
+}
+console.log(person);
+console.log(typeof person);
+
+//array
+
+let fruits = ['apple', 'banana'];
+console.log(fruits);
+console.log(typeof fruits);
+//function
+let greet = function () {
+    console.log("Namste!");
+}
+console.log(greet());
+console.log(typeof greet);
---
+++ Lecture-05/script.js
@@ -0,0 +1,30 @@
+function greet() {
+    if (true) {
+        var greetMessage;
+        greetMessage = "Good Morning!"
+    }
+    console.log(greetMessage);
+}
+//console.log(greetMessage);
+greet();
+
+function greet1() {
+    let greetMessage;
+  if (true) {
+      //let greetMessage = "Good Morning!";
+    //   console.log(greetMessage);
+    }
+    greetMessage = "Good Night!"
+    console.log(greetMessage);
+  
+}
+
+greet1();
+{
+    const birthYear = 2003;
+
+    console.log(birthYear);
+}
+
+//birthYear = 2006;
+
