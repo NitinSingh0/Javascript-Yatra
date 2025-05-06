87d187c968cd93809fa5d8ac36d66f350c9eebf3
NitinSingh0 <31nitinthakur@gmail.com>
2025-05-06 19:05:10 +0530

    Lecture-06 and 07

---
+++ Lecture-06/script.js
@@ -0,0 +1,22 @@
+const firstName = "Dev";
+const job = "Student";
+const birthYear = "2003";
+const year = '2025';
+
+let message = "I'm " + firstName + " a " + (year - birthYear) + " year old " + job;
+console.log(message);
+
+message = `I'm ${firstName} a ${year - birthYear} year old ${job}`;
+console.log(message);
+
+console.log("Multiple \n\
+    line \n\
+    string");
+
+console.log(`multiple
+    line
+    string`);
+
+console.log(`normal string`);
+
+
